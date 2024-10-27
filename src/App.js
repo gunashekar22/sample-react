4eb623f056916a73fc7a3daf1402068997920048
gunashekhar22 <undabatlagunashekar25253@gmail.com>
2024-10-27 18:49:02 +0530

    5th

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-          welocme to react
+          
         </a>
       </header>
     </div>
